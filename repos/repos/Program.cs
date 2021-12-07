69d02b4335d8ac2542ad8654adabbd3ab8bab0f0
marwanhalim0@gmail.com <marwanhalim0@gmail.com>
2021-12-07 09:41:44 +0800

    Membuat logic notasi algoritma (20.11.3466 Marwan Halim)

---
+++ repos/repos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tugas GIT Kelompok");
+            Console.WriteLine("Membuat Notasi Aritmatika");
+
+            int x, y;
+            x = 10;
+            y = 5;
+
+            Console.ReadKey();
+        }
+    }
+}
